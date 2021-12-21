ddefc45fec682d2ebad36c76f740156bd4e6b7cd
vkaswin <vkaswin1998@gmail.com>
2021-12-21 20:55:33 +0530

    update

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { useAuth, useRouter } from "hooks";
 
 const SignIn = () => {
-  const router = useRouter();
-
-  const { setUser } = useAuth();
-
   return (
     <div>
       <h1>Sign In</h1>
